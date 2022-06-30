1911225256553cf379daab66b220d97beb3df01b
mohamedMallahi <kingcoding17@gmail.com>
2022-06-30 11:32:38 +0100

    Add a page for sing post

---
+++ pages/api/[id].js
@@ -0,0 +1,48 @@
+import Head from 'next/head';
+
+export default function Blogs({ article }) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Welcome To NetBlogger! Start editing to see some magic happen :)"
+        />
+        <title>{article.title}</title>
+      </Head>
+      <div className="article">
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch(
+    `http://jsonplaceholder.typicode.com/posts/${context.params.id}`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      },
+    }
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      article: data,
+    },
+  };
+}
+
+export async function getStaticPath() {
+  const res = await fetch('http://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return {
+    paths: [data.map((post) => ({ params: { id: post.id } }))],
+    fallback: false,
+  };
+}
