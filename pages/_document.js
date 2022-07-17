168be812dd5e2011376b1c806a4933fb8cb0ef1c
mohamedMallahi <kingcoding17@gmail.com>
2022-07-17 14:49:33 +0100

    1 file modified

--- pages/_document.js
+++ pages/_document.js
@@ -15,13 +15,11 @@ export default function MyDocument() {
           rel="stylesheet"
         />
         <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossOrigin="anonymous"
+          href="https://bootswatch.com/5/litera/bootstrap.min.css"
         />
       </Head>
-      <body>
+      <body className="bg-primary">
         <Main />
         <NextScript />
       </body>
