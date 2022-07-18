5936c4c608bcf9023f74f7cfc1ec8b3f0bfc30d4
mohamedMallahi <kingcoding17@gmail.com>
2022-07-18 11:24:19 +0100

    fixing a bug

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import useRouter from 'next/router';
+import { useRouter } from 'next/router';
 
 import getArticles from '../../utils/getArticles';
 import { useAuth } from '../../contexts/AuthContext';
