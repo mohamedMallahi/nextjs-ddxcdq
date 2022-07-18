54f5e2319182e541a83d04665836341d295b7064
mohamedMallahi <kingcoding17@gmail.com>
2022-07-18 11:21:19 +0100

    1 file modified

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -11,49 +11,49 @@ import Stack from 'react-bootstrap/Stack';
 
 export default function Admin({ articles }) {
   const { user } = useAuth();
-  const router = useRouter;
+  const router = useRouter();
 
   if (!user) {
-    router.push('/signin');
-  } else {
-    return (
-      <>
-        <Head>
-          <meta name="robots" content="noindex" />
-        </Head>
-        <Table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Published At</th>
-              <th>Tags</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {articles.map((article) => (
-              <tr>
-                <td>{article.title}</td>
-                <td>2022</td>
-                <td>
-                  <Stack direction="horizontal" gap={2}>
-                    <Badge bg="secondary">Ninja</Badge>
-                    <Badge bg="secondary">Dragon Ball</Badge>
-                  </Stack>
-                </td>
-                <td>
-                  <Stack direction="horizontal" gap={2}>
-                    <Button>Edit</Button>
-                    <Button className="m">Delete</Button>
-                  </Stack>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </>
-    );
+    return router.push('/signin');
   }
+
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Published At</th>
+            <th>Tags</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles.map((article) => (
+            <tr>
+              <td>{article.title}</td>
+              <td>2022</td>
+              <td>
+                <Stack direction="horizontal" gap={2}>
+                  <Badge bg="secondary">Ninja</Badge>
+                  <Badge bg="secondary">Dragon Ball</Badge>
+                </Stack>
+              </td>
+              <td>
+                <Stack direction="horizontal" gap={2}>
+                  <Button>Edit</Button>
+                  <Button className="m">Delete</Button>
+                </Stack>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
 }
 
 export const getStaticProps = async () => {
