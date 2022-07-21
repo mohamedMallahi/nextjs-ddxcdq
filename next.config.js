7e554bc127e871f4b580866bce03a6bf6a8483f4
mohamedMallahi <kingcoding17@gmail.com>
2022-07-21 11:36:53 +0100

    1 file modified

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['images.ctfassets.net', 'images.pexels.com'],
+    domains: ['images.ctfassets.net', 'images.pexels.com', 'www.pexels.com'],
     disableStaticImages: true,
   },
 };
